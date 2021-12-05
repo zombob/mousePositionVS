8dcbcaea5f4ebe0cd16fe6fe43ae49975c272f1c
xin <xin@zhiyoung.com>
2021-12-05 22:24:38 +0800

    添加项目文件。

---
+++ mousePosition.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include<windows.h>
+
+int main() {
+    POINT p;
+    p.x = 0;
+    p.y = 0;
+    while (1) {
+        POINT cp;
+        GetCursorPos(&cp);
+        if ((cp.x != p.x) or (cp.y != p.y)) {
+            p.x = cp.x;
+            p.y = cp.y;
+            std::cout << "X: " << p.x << "\tY: " << p.y << std::endl;
+        }
+        Sleep(10);
+    }
+    return 0;
+}
